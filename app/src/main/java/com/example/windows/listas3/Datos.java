a416f359a59c42f3053921a547026b48d751941a
listas <soul53@gmail.com>
2018-02-15 18:48:08 -0700

    estas son las tres primeras pantallas

---
+++ app/src/main/java/com/example/windows/listas3/Datos.java
@@ -0,0 +1,12 @@
+package com.example.windows.listas3;
+
+import java.util.ArrayList;
+
+/**
+ * Created by windows on 15-feb-18.
+ */
+
+public class Datos {
+
+
+}
---
+++ app/src/main/java/com/example/windows/listas3/login.java
@@ -0,0 +1,27 @@
+package com.example.windows.listas3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, formulario.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}

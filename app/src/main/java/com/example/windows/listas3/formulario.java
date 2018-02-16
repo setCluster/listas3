aa3e66ae8b4d1fc542a2a49569ad3565c16fca4a
listas <soul53@gmail.com>
2018-02-16 10:03:50 -0700

    Arreglado la navegacion desde los botones,el problema era una redundancia en las sentencia del intent

--- app/src/main/java/com/example/windows/listas3/formulario.java
+++ app/src/main/java/com/example/windows/listas3/formulario.java
@@ -1,7 +1,12 @@
 package com.example.windows.listas3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class formulario extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class formulario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+    }
+
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Menu_principal.class);
+        startActivity(intent);
     }
 }
